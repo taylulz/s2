90bbba3dd51746671ec26f4aec23f58b68fadf9f
taylor d <taylor.delph@eyecuelab.com>
2021-11-18 08:28:50 -0800

    navigate to index after delete invoice

---
+++ src/routes/invoice.jsx
@@ -0,0 +1,29 @@
+import { useParams, useNavigate } from "react-router";
+import { getInvoice, deleteInvoice } from "../data";
+
+export default function Invoice() {
+  let navigate = useNavigate();
+  let params = useParams();
+  let invoice = getInvoice(parseInt(params.invoiceId, 10));
+  
+  return (
+    <main style={{ padding: "1rem" }}>
+      <h2>Total Due: {invoice.amount}</h2>
+      <p>
+        {invoice.name}: {invoice.number}
+      </p>
+      <p>Due Date: {invoice.due}</p>
+      <p>
+        <button
+          onClick={() => {
+            deleteInvoice(invoice.number);
+            navigate("/invoices");
+            // navigate("/expenses");
+          }}
+        >
+          Delete
+        </button>
+      </p>
+    </main>
+  );
+}

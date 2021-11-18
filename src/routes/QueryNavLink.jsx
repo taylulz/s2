2d8998da89af43d0c56c425f65aa78cf74774f4c
taylor d <taylor.delph@eyecuelab.com>
2021-11-18 08:15:11 -0800

    add in notes for posterity

--- src/routes/QueryNavLink.jsx
+++ src/routes/QueryNavLink.jsx
@@ -1,8 +1,18 @@
-import { useLocation, NavLink } from "react-router-dom";
+import { useLocation, NavLink, useSearchParams } from "react-router-dom";
 
 function QueryNavLink({ to, ...props }) {
   let location = useLocation();
   return <NavLink to={to + location.search} {...props} />;
 }
+
+// useLocation, like useSearchParams, returns a location that tells us info about the URL. a location look something like this:
+
+// {
+//   pathame: "/invoices",
+//   search: "?filter=sa",
+//   hash: "",
+//   state: null,
+//   key: "ae4cz2j"
+// }
 
 export default QueryNavLink

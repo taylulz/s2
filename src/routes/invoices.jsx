dea5db729ad3f5c0f713d949c62eff584a1bb357
taylor d <taylor.delph@eyecuelab.com>
2021-11-17 21:46:44 -0800

    change link color w/function return isActive

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,24 +1,30 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import { getInvoices } from "../data";
 
 export default function Invoices() {
   let invoices = getInvoices();
   return (
     <div style={{ display: "flex" }}>
-      <nav 
-        style={{ 
+      <nav
+        style={{
           borderRight: "solid 1px",
-          padding: "1rem" 
+          padding: "1rem"
         }}
       >
         {invoices.map(invoice => (
-          <Link
-            style={{ display: "block", margin: "1rem 0" }}
+          <NavLink
+            style={({ isActive }) => {
+              return {
+                display: "block",
+                margin: "1rem 0",
+                color: isActive ? "red" : ""
+              };
+            }}
             to={`/invoices/${invoice.number}`}
             key={invoice.number}
           >
             {invoice.name}
-          </Link>
+          </NavLink>
         ))}
       </nav>
       <Outlet />

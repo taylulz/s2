75e6f563a0db98f13465d7ce572f314957d678de
taylor d <taylor.delph@eyecuelab.com>
2021-11-17 21:19:24 -0800

    get :invoiceId param from the URL

--- src/routes/invoice.js
+++ src/routes/invoice.js
@@ -1,3 +1,6 @@
+import { useParams } from "react-router";
+
 export default function Invoice() {
-  return <h2>Invoice #???</h2>;
+  let params = useParams();
+  return <h2>Invoice: {params.invoiceId}</h2>;
 }

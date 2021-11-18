88fbc5db2cc7593b843cc6cc57829e664431674b
taylor d <taylor.delph@eyecuelab.com>
2021-11-17 21:56:41 -0800

    url search filter params

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,8 +1,14 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { 
+  NavLink, 
+  Outlet,
+  useSearchParams
+} from "react-router-dom";
 import { getInvoices } from "../data";
 
 export default function Invoices() {
   let invoices = getInvoices();
+  let [searchParams, setSearchParams] = useSearchParams();
+
   return (
     <div style={{ display: "flex" }}>
       <nav
@@ -11,7 +17,25 @@ export default function Invoices() {
           padding: "1rem"
         }}
       >
-        {invoices.map(invoice => (
+        <input
+          value={searchParams.get("filter") || ""}
+          onChange={event => {
+            let filter = event.target.value;
+            if (filter) {
+              setSearchParams({ filter });
+            } else {
+              setSearchParams({});
+            }
+          }}
+        />
+        {invoices
+          .filter(invoice => {
+            let filter = searchParams.get("filter");
+            if(!filter) return true;
+            let name = invoice.name.toLowerCase();
+            return name.startsWith(filter.toLowerCase());
+          })
+          .map(invoice => (
           <NavLink
             style={({ isActive }) => {
               return {
